193e57ad26c2dcab8d0d00236eaf4efb67ed361c
SyedAli999 <114101483+SyedAli999@users.noreply.github.com>
2022-09-26 12:08:41 -0600

    Update sketch.js

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -6,11 +6,34 @@
 // - describe what you did to take this project "above and beyond"
 
 
+let x, y, squareSize, speed;
+let dx = 3;
+let dy = 5;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1200, 1200);
+  x = 0;
+  y = 0;
+  squareSize = 50;
+  speed = 5;
 }
 
 function draw() {
   background(220);
-  square(100, 100, 50);
+  moveSquare();
+drawSquare;
+
+ 
+}
+
+function moveSquare() {
+  if (state === 'right') {}
+  x += speed;
+
+
+}
+
+
+function drawSquare() {
+  BACK
 }
